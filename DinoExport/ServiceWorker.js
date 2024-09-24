464f49da3395dea7a92bc0d060f5ad060f2e7104
bach <huy.dq161801@gmail.com>
2024-09-24 22:22:09 +0700

    - Update

---
+++ DinoExport/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Dino-Dino-0.1";
+const contentToCache = [
+    "Build/DinoExport.loader.js",
+    "Build/DinoExport.framework.js.br",
+    "Build/DinoExport.data.br",
+    "Build/DinoExport.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
