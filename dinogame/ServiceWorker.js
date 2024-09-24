117a4e43fa3437060682d1be420cf014d704ed2a
bach <huy.dq161801@gmail.com>
2024-09-24 23:20:51 +0700

    -Update

--- dinogame/ServiceWorker.js
+++ dinogame/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Dino-Dino-0.1";
+const cacheName = "dino-dino-0.1";
 const contentToCache = [
     "Build/dinogame.loader.js",
-    "Build/dinogame.framework.js.br",
-    "Build/dinogame.data.br",
-    "Build/dinogame.wasm.br",
+    "Build/dinogame.framework.js",
+    "Build/dinogame.data",
+    "Build/dinogame.wasm",
     "TemplateData/style.css"
 
 ];
